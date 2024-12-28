5e6b59855a775f5eb09df65ad63c9e1cc368dd44
nadnadn <maryunani52@gmail.com>
2024-12-28 23:54:44 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+let nama = ('allooo alell, aku lg bljr ngoding anjai masi dasar bgt ini wkwkwk aiss susah weh sisfor pls be nice heheh, next next ')
+alert(`${nama}`)
+let usia = prompt('jawab jujurr km umur berapa?')
+alert(`hahh ${usia} ??? boonk bukannya masi bayikk yaa? aisss`)
+let pacar = prompt('nama lengkap aku siapa rell hm hm hm? (harus capslock!!! wkwk)')
+ 
+if (pacar == 'NADYA DWI RAHMADHANI') {
+    pacar = 'heheh betul'
+} else {
+    pacar = 'aisssss salah (emot ngambek)'
+}
+alert(`${pacar}`)
+
+let closing = ('mmm aku mau bilang sesuatuu')
+let lanjut = ('kepoooo yaa? sukurinnnnn wleee')
+let serius = ('heheh')
+let bilang = ('bilang ga yaa, ntar km geer abiss')
+let kgn = ('aku kangen.')
+let slt = ('ais kamu bacanya sambil senyum gaa?')
+let tdr = ('udah wes cape mataku natap leptop, ribet anjai ngoding ni pdhl masi dasar aiss bisa ga ya jadi pro player wkwkwk aiss')
+let langsung = ('udah wes mau tidorrr, ngantuk')
+let syg = ('bye sayaaang')
+
+alert(`${closing}`)
+alert(`${lanjut}`)
+alert(`${serius}`)
+alert(`${bilang}`)
+alert(`${kgn}`)
+alert(`${slt}`)
+alert(`${tdr}`)
+alert(`${langsung}`)
+alert(`${syg}`)
+
